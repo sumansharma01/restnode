e33b98b29278f86dabab55918f7868271e6e2c01
sumansharma01 <suman.sharma0331@gmail.com>
2021-04-02 17:57:27 +0530

    handled exceptions

--- app.js
+++ app.js
@@ -9,4 +9,22 @@ const ordersRoute=require('./routes/orders');
 app.use('/products',productsRoute);
 app.use('/orders',ordersRoute);
 
+app.use((req,res,next)=>{
+    const error=new Error("404 resource not found");
+    error.status=404;
+    next(error);
+
+})
+
+app.use((error,req,res,next)=>{
+    res.status(error.status || 500);
+    res.json(
+        {
+            error:{
+                message:error.message
+            }
+        }
+    )
+})
+
 module.exports=app;
