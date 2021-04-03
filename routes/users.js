575fa1db13b0d201ae405cd4f6ada278812505fe
sumansharma01 <suman.sharma0331@gmail.com>
2021-04-03 15:03:30 +0530

    added login function

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const mongoose=require('mongoose');
 const express=require('express');
 const router=express.Router();
+const jwt=require('jsonwebtoken');
 
 const User=require('../models/UsersModel');
 
@@ -56,6 +57,72 @@ router.post('/signup',(req,res,next)=>{
 
 
 
+})
+
+
+//deleting particular user
+router.delete('/:id',(req,res,next)=>{
+    const id=req.params.id;
+    User.findById(id).then((data)=>{
+        if(data){
+        User.deleteOne({_id:id})
+        .then((data)=>{
+            res.status(200).json({
+                message:"user deleted"
+            })
+        })
+        .catch((err)=>{
+            res.status(500).json({
+                message:"error in deletion of user"
+            });
+        }
+        )}
+        else{
+            res.status(500).json({
+                message:"no user found"
+            })
+        }
+    })
+    .catch((err)=>{
+        res.status(500).json({
+            message:"no user found"
+        })
+    })
+    
+    
+    
+})
+
+
+
+
+
+
+router.post('/login',(req, res,next)=>{
+    User.find({email:req.body.email})
+    .then((user)=>{
+        bcrypt.compare(req.body.password,user[0].password,(err,result)=>{
+            if(result){
+                const token=jwt.sign({
+                        id:user[0]._id,
+                        email:user[0].email
+                    }
+                    ,process.env.JWT_PRIVATE_KEY,
+                    {
+                        expiresIn:"1h"
+                    })
+                return res.status(200).json({
+                    message:"Auth successful",
+                    token:token
+                })
+            }
+        })
+    })
+    .catch((err)=>{
+        res.status(402).json({
+            message:"Auth failed"
+        })
+    })
 })
 
 module.exports=router;
